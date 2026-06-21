bd6176a2948d903849e9dbd3ebbad9c94fd2fa2a
Rafael Sousa <raffael_sousa@icloud.com>
2026-06-21 00:23:25 -0300

    Construcao de display da pagina de detalhes da crianca

--- src/app/child-detail/child-detail.page.ts
+++ src/app/child-detail/child-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonList, IonItem, IonLabel } from '@ionic/angular/standalone';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-child-detail',
@@ -11,10 +12,13 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonList, IonIte
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonList, IonItem, IonLabel, CommonModule, FormsModule]
 })
 export class ChildDetailPage implements OnInit {
+  childId: string | null = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.childId = this.route.snapshot.paramMap.get('id');
+    console.log('ID da Criança:', this.childId);
   }
 
 }
